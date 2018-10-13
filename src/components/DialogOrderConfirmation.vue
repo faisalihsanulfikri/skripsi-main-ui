17e165f5d3e843a4db2af394bff4bfc09be9c825
irsankirim-at-050764449592 <irsangunawan@Irsans-MBP-2.(none)>
2018-10-13 10:22:46 +0700

    order confirm

---
+++ src/components/DialogOrderConfirmation.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-dialog
+    title="Konfirmasi Order"
+    :visible="visible"
+    @close="close">
+    <form>
+      <div class="uk-margin">
+        <label class="uk-form-label">Anda akan melakukan transaksi sebesar Rp xx.xxx.xxx </label>
+      </div>
+      <div class="uk-margin">
+        <label class="uk-form-label">Silahkan lakukan pembayaran ke nomor rekening berikut </label>
+      </div>
+      <div class="uk-margin">
+        <label class="uk-form-label">BCA: xxxxxxxx (PT. Kirimin) </label>
+      </div>
+    </form>
+    <h4>{{ message }}</h4>
+    <div slot="footer">
+      <button class="uk-button uk-button-danger" @click="confirm">YA</button>
+      <button class="uk-button uk-button-default uk-margin-small-left" @click="close">TIDAK</button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      default: false
+    },
+    message: {
+      default: 'Lanjutkan transaksi ini?'
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    },
+    confirm () {
+      this.$emit('confirm')
+    }
+  }
+}
+</script>
