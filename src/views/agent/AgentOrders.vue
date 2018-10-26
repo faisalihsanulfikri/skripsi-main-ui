745bd6aa3b12cfeaaf6b0f995d0d8158b48f18ad
irsankirim-at-050764449592 <irsangunawan@Irsans-MBP-2.(none)>
2018-10-26 23:28:50 +0700

    agent order

--- src/views/agent/AgentOrders.vue
+++ src/views/agent/AgentOrders.vue
@@ -48,7 +48,7 @@
                           <table>
                             <thead>
                             <tr>
-                              <th>No</th
+                              <th>No</th>
                               <th>Nama</th>
                               <th>Berat</th>
                               <th>Harga</th>
@@ -70,7 +70,7 @@
                                 <td>{{ item.width }} {{ item.vunits }}</td>
                                 <td>{{ item.height }} {{ item.vunits }}</td>
                                 <td>
-                                  <button class="uk-button uk-button-primary" type="button" >Konfirmasi</button>
+                                  <button class="uk-button uk-button-primary" type="button" >Confirm</button>
                                 </td>
                                 <td>
                                   <button class="uk-button uk-button-danger" type="button" @click="print">Print AWB</button>
