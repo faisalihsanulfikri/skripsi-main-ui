8348ee093c5a07d338ac0106a6dc89e03c74bd49
faisalihsanulfikri <faisal@kirimin.co.id>
2019-03-22 09:56:58 +0700

    fix order report

--- src/views/agent/report/Order.vue
+++ src/views/agent/report/Order.vue
@@ -14,36 +14,37 @@
             </h3>
           </div>
         </div>
-        <div class="uk-width-auto">
-        </div>
+        <div class="uk-width-auto"></div>
       </div>
     </div>
     <div class="uk-card-body uk-card-small">
       <div class="uk-margin">
         <div class="uk-grid-small" uk-grid>
           <div class="uk-width-auto">
-            <el-date-picker v-model="filter.time" type="daterange" format="yyyy-MM-dd" value-format="yyyy-MM-dd"
-              range-separator="To" start-placeholder="Start date" end-placeholder="End date">
-            </el-date-picker>
+            <el-date-picker
+              v-model="filter.time"
+              type="daterange"
+              format="yyyy-MM-dd"
+              value-format="yyyy-MM-dd"
+              range-separator="To"
+              start-placeholder="Start date"
+              end-placeholder="End date"
+            ></el-date-picker>
           </div>
           <div class="uk-width-auto">
             <el-button type="default" @click="fetchOrderReport">Filter</el-button>
           </div>
           <div class="uk-width-1-3 uk-margin-auto-left">
             <el-input v-model="filter.search" placeholder="Search...">
-              <el-button slot="append" icon="el-icon-search" @click="fetchOrderReport">
-              </el-button>
+              <el-button slot="append" icon="el-icon-search" @click="fetchOrderReport"></el-button>
             </el-input>
           </div>
           <div class="uk-width-1-1" style="padding-top:10px">
-
             <el-button type="default" @click="exportReportXLSX">
-              <font-awesome-icon icon="file-excel"></font-awesome-icon>
-              Download XLSX
+              <font-awesome-icon icon="file-excel"></font-awesome-icon>Download XLSX
             </el-button>
             <el-button type="default" @click="exportReportCSV">
-              <font-awesome-icon icon="file-excel"></font-awesome-icon>
-              Download CSV
+              <font-awesome-icon icon="file-excel"></font-awesome-icon>Download CSV
             </el-button>
           </div>
         </div>
@@ -79,79 +80,83 @@
 </template>
 
 <script>
-  import moment from 'moment'
-  import saveAs from 'file-saver'
+import moment from "moment";
+import saveAs from "file-saver";
 
-  export default {
-    data() {
-      return {
-        orders: [],
-        filter: {
-          time: [],
-          search: ''
-        }
+export default {
+  data() {
+    return {
+      orders: [],
+      filter: {
+        time: [],
+        search: ""
       }
-    },
+    };
+  },
 
-    created() {
-      this.filter.time = [
-        moment().startOf('month').format('YYYY-MM-DD'),
-        moment().endOf('month').format('YYYY-MM-DD')
-      ]
+  created() {
+    this.filter.time = [
+      moment()
+        .startOf("month")
+        .format("YYYY-MM-DD"),
+      moment()
+        .endOf("month")
+        .format("YYYY-MM-DD")
+    ];
 
-      this.fetchOrderReport()
-    },
+    this.fetchOrderReport();
+  },
 
-    methods: {
-      async exportReportCSV() {
-        this.__startLoading()
+  methods: {
+    async exportReportCSV() {
+      this.__startLoading();
 
-        try {
-          let res = await this.$service.report.orderExportCSV(this.filter)
+      try {
+        let res = await this.$service.report.orderExportCSV(this.filter);
 
-          let content = res.request.getResponseHeader('Content-Disposition')
-          let regexResult = content.match('filename=(.*)')
-          let filename = regexResult[1].replace(new RegExp('"', 'g'), '')
-          let blob = new Blob([res.data])
+        let content = res.request.getResponseHeader("Content-Disposition");
+        let regexResult = content.match("filename=(.*)");
+        let filename = regexResult[1].replace(new RegExp('"', "g"), "");
+        let blob = new Blob([res.data]);
 
-          saveAs(blob, filename)
-        } catch (err) {
-          this.__handleError(this, err, true)
-        }
-
-        this.__stopLoading()
-      },
-      async exportReportXLSX() {
-        this.__startLoading()
-
-        try {
-          let res = await this.$service.report.orderExportXLSX(this.filter)
-
-          let content = res.request.getResponseHeader('Content-Disposition')
-          let regexResult = content.match('filename=(.*)')
-          let filename = regexResult[1].replace(new RegExp('"', 'g'), '')
-          let blob = new Blob([res.data])
-
-          saveAs(blob, filename)
-        } catch (err) {
-          this.__handleError(this, err, true)
-        }
-
-        this.__stopLoading()
-      },
-      async fetchOrderReport() {
-        this.__startLoading()
-
-        try {
-          let res = await this.$service.report.order(this.filter)
-
-          this.orders = res.data
-        } catch (err) {
-          this.__handleError(this, err, true)
-        }
-
-        this.__stopLoading()
+        saveAs(blob, filename);
+      } catch (err) {
+        this.__handleError(this, err, true);
       }
+
+      this.__stopLoading();
+    },
+    async exportReportXLSX() {
+      this.__startLoading();
+
+      try {
+        let res = await this.$service.report.orderExportXLSX(this.filter);
+
+        let content = res.request.getResponseHeader("Content-Disposition");
+        let regexResult = content.match("filename=(.*)");
+        let filename = regexResult[1].replace(new RegExp('"', "g"), "");
+        let blob = new Blob([res.data]);
+
+        saveAs(blob, filename);
+      } catch (err) {
+        this.__handleError(this, err, true);
+      }
+
+      this.__stopLoading();
+    },
+    async fetchOrderReport() {
+      this.__startLoading();
+
+      try {
+        let res = await this.$service.report.order(this.filter);
+
+        this.orders = res.data;
+      } catch (err) {
+        this.__handleError(this, err, true);
+      }
+
+      this.__stopLoading();
     }
   }
+};
 </script>
