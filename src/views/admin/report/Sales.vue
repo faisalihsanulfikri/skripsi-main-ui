31310eac730391f457488c9905adcf49ef55e4c8
rizal151 <rizaladityoo@yahoo.com>
2019-04-11 10:06:51 +0700

    change width table report sales

--- src/views/admin/report/Sales.vue
+++ src/views/admin/report/Sales.vue
@@ -60,18 +60,18 @@
               <th>Customer</th>
               <!-- <th>Consolidate</th> -->
               <!-- <th>Total Packages</th> -->
-              <th class="uk-table-expand">Total Item`s</th>
+              <th>Total Item`s</th>
               <!-- <th>WEIGHT</th> -->
-              <th class="uk-table-expand">Item Price</th>
-              <th class="uk-table-expand">Shipping Cost</th>
+              <th class="uk-table-expand min-width">Item Price</th>
+              <th class="uk-table-expand min-width">Shipping Cost</th>
               <!-- <th>Domestic Cost</th>
               <th>Incoming Duty</th>
               <th>PPN</th>
               <th>PPH</th> -->
-              <th class="uk-table-expand">Tax</th>
-              <th class="uk-table-expand">Insurance</th>
+              <th class="uk-table-expand min-width">Tax</th>
+              <th class="uk-table-expand min-width">Insurance</th>
               <!-- <th>Packagin Cost</th> -->
-              <th class="uk-table-expand">Total</th>
+              <th class="uk-table-expand min-width">Total</th>
               <!-- <th>Status</th> -->
             </tr>
           </thead>
@@ -230,7 +230,7 @@ export default {
     text-align: right;
 
   }
-  .auto{
-
+  .min-width{
+    min-width: 125px !important;
   }
 </style>
