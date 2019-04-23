a5a5c57eb6d3fd48ec4006a24870fa7571f5d532
Andri Firmansyah <andrifmsyh@gmail.com>
2019-04-23 09:21:08 +0700

    Add PageTitle component to Inbound Page

--- src/views/admin/order/Inbound.vue
+++ src/views/admin/order/Inbound.vue
@@ -1,23 +1,8 @@
 <template>
   <div class="uk-card uk-card-default">
-    <div class="uk-card-header app--card-header">
-      <div uk-grid>
-        <div class="uk-width-auto">
-          <div class="app--card-header__icon">
-            <font-awesome-icon icon="file-alt"></font-awesome-icon>
-          </div>
-        </div>
-        <div class="uk-width-expand">
-          <div class="app--card-header_title">
-            <h3>
-              <span>Inbound</span>
-              <el-badge :value="pagination.total"></el-badge>
-            </h3>
-          </div>
-        </div>
-        <div class="uk-width-auto"></div>
-      </div>
-    </div>
+    <!-- Header -->
+    <PageTitle title="Inbound" :paginationTotal="pagination.total"/>
+
     <div class="uk-card-body uk-card-small">
       <div class="uk-margin uk-grid-small" uk-grid>
         <div class="uk-width-auto">
@@ -277,12 +262,13 @@
 </template>
 
 <script>
-// import moment from "moment";
+import PageTitle from "@/components/PageTitle";
 import CalculatorResult from "../../../components/CalculatorResult";
 
 export default {
   components: {
-    CalculatorResult
+    CalculatorResult,
+    PageTitle
   },
 
   data() {
