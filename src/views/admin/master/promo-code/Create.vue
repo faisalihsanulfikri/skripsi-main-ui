bfad21374f26ff38a40f4dde93f5276c06cea540
faisalihsanulfikri <faisal@kirimin.co.id>
2019-05-04 20:44:21 +0700

    fix form add promo code

--- src/views/admin/master/promo-code/Create.vue
+++ src/views/admin/master/promo-code/Create.vue
@@ -23,43 +23,95 @@
       </div>
       <div class="uk-margin">
         <label class="uk-form-label">Status</label>
-        <el-input v-model="input.status"></el-input>
+        <div>
+          <select v-model="input.status" name="status" class="uk-select" @change="onStatusChanged">
+            <option
+              v-for="(status, i) in options.status"
+              :key="i"
+              :value="status.value"
+            >{{ status.label }}</option>
+          </select>
+        </div>
       </div>
       <div class="uk-margin">
-        <label class="uk-form-label">Status</label>
-        <el-input v-model="input.status"></el-input>
+        <label class="uk-form-label">Start Date</label>
+        <el-input v-model="input.start_date"></el-input>
+      </div>
+      <div class="uk-margin">
+        <label class="uk-form-label">End Date</label>
+        <el-input v-model="input.end_date"></el-input>
       </div>
       <div class="uk-margin">
         <label class="uk-form-label">Description</label>
         <el-input v-model="input.description" type="textarea" rows="4"></el-input>
       </div>
+
+      <!--  -->
       <div class="uk-margin">
-        <label class="uk-form-label">Required Document</label>
+        <label class="uk-form-label">Unlimited</label>
 
         <div class="req-doc">
           <label class="btn-r">
             <input
-              v-model="input.document"
+              v-model="input.unlimited"
               class="uk-radio"
               type="radio"
-              value="yes"
-              @click="onDocumentChanged"
+              value="true"
+              @click="onUnlimitedChanged"
             >
-            <span class="uk-margin-small-left">Yes</span>
+            <span class="uk-margin-small-left">True</span>
           </label>
 
           <label class="btn-r">
             <input
-              v-model="input.document"
+              v-model="input.unlimited"
               class="uk-radio"
               type="radio"
-              value="No"
-              @click="onDocumentChanged"
+              value="false"
+              @click="onUnlimitedChanged"
             >
-            <span class="uk-margin-small-left">No</span>
+            <span class="uk-margin-small-left">False</span>
           </label>
         </div>
       </div>
+      <!--  -->
+      <div class="uk-margin">
+        <label class="uk-form-label">Capacity</label>
+        <el-input v-model="input.capacity"></el-input>
+      </div>
+      <div class="uk-margin">
+        <label class="uk-form-label">Promo Type</label>
+        <div>
+          <select
+            v-model="input.promo_type"
+            name="status"
+            class="uk-select"
+            @change="onPromoTypeChanged"
+          >
+            <option
+              v-for="(status, i) in options.promo_type"
+              :key="i"
+              :value="status.value"
+            >{{ status.label }}</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="uk-margin" v-if="input.promo_type == 'fixed'">
+        <label class="uk-form-label">Value {{input.promo_type}}</label>
+        <el-input v-model="input.value_fixed"></el-input>
+      </div>
+
+      <div class="uk-margin" v-else-if="input.promo_type == 'percentage'">
+        <label class="uk-form-label">Value {{input.promo_type}}</label>
+        <el-input v-model="input.value_percentage"></el-input>
+      </div>
+
+      <div class="uk-margin" v-else-if="input.promo_type == 'point'">
+        <label class="uk-form-label">Value {{input.promo_type}}</label>
+        <el-input v-model="input.value_point"></el-input>
+      </div>
+
       <el-alert v-if="error" title="ERROR" type="error" :description="errorMessage" show-icon></el-alert>
     </div>
     <div class="uk-card-footer uk-text-right">
@@ -75,13 +127,27 @@ export default {
       edit: false,
       title: "New Promo Code",
       input: {
-        name: "",
+        code: "",
+        status: "",
+        start_date: "",
+        end_date: "",
         description: "",
-        document: ""
+        unlimited: "",
+        capacity: "",
+        in_used: "",
+        promo_type: "",
+        value_fixed: "",
+        value_percentage: "",
+        value_point: ""
+      },
+      master: {
+        statuses: [],
+        promo_types: [],
+        unlimiteds: []
       },
       options: {
         status: [],
-        promoType: [],
+        promo_type: [],
         unlimited: []
       },
       error: false,
@@ -91,11 +157,15 @@ export default {
 
   created() {
     this.setOptions();
+
+    // console.log("master", this.master.statuses);
+    // console.log("options", this.options.status);
+
     if (this.$route.params.id) {
       this.edit = true;
       this.title = "Edit Promo Code";
 
-      this.getCategory();
+      // this.getCategory();
     }
 
     console.log(this.$route.params.id);
@@ -103,43 +173,87 @@ export default {
 
   methods: {
     setOptions() {
-      this.options.status = [
-        { value: "active", lable: "Active" },
-        { value: "suspend", lable: "Suspend" },
-        { value: "inactive", lable: "Inactive" }
+      // masters
+      this.master.statuses = [
+        { value: "active", label: "Active" },
+        { value: "suspend", label: "Suspend" },
+        { value: "inactive", label: "Inactive" }
       ];
-      this.options.promoType = [
-        { value: "fixed", lable: "Fixed" },
-        { value: "percentage", lable: "Percentage" },
-        { value: "point", lable: "Point" }
+
+      this.master.promo_types = [
+        { value: "fixed", label: "Fixed" },
+        { value: "percentage", label: "Percentage" },
+        { value: "point", label: "Point" }
       ];
-      this.options.unlimited = [
-        { value: true, lable: "True" },
-        { value: false, lable: "False" }
+
+      this.master.unlimiteds = [
+        { value: true, label: "True" },
+        { value: false, label: "False" }
       ];
+
+      // options
+      this.options.status = this.master.statuses.map(item => {
+        let $item = {
+          value: item.value,
+          label: item.label
+        };
+        return $item;
+      });
+
+      this.options.promo_type = this.master.promo_types.map(item => {
+        let $item = {
+          value: item.value,
+          label: item.label
+        };
+        return $item;
+      });
+
+      this.input.status = this.options.status[0].value;
+      this.input.promo_type = this.options.promo_type[0].value;
     },
-    onDocumentChanged() {
+    onStatusChanged() {
+      let status = this.master.statuses.find(status => {
+        return statuses.value === this.input.status;
+      });
+      this.input.status = statuses.value;
+    },
+
+    onPromoTypeChanged() {
+      let promo_type = this.master.promo_types.find(status => {
+        return promo_types.value === this.input.promo_type;
+      });
+      this.input.promo_type = promo_types.value;
+    },
+
+    // onUnlimitedChanged() {
+    //   let status = this.master.statuses.find(status => {
+    //     return statuses.value === this.input.status;
+    //   });
+
+    //   this.input.status = statuses.value;
+    // },
+    onUnlimitedChanged() {
       if (this.input.length > 0) {
-        this.input.document = "";
+        this.input.unlimited = "";
       }
     },
-    async getCategory() {
-      this.__startLoading();
+    // async getCategory() {
+    //   this.__startLoading();
 
-      this.error = false;
-      this.errorMessage = "";
+    //   this.error = false;
+    //   this.errorMessage = "";
 
-      try {
-        let res = await this.$service.category.find(this.$route.params.id);
+    //   try {
+    //     let res = await this.$service.category.find(this.$route.params.id);
 
-        this.input.name = res.data.name;
-        this.input.description = res.data.description;
-      } catch (err) {
-        this.__handleError(this, err, true);
-      }
+    //     this.input.name = res.data.name;
+    //     this.input.description = res.data.description;
+    //   } catch (err) {
+    //     this.__handleError(this, err, true);
+    //   }
 
-      this.__stopLoading();
-    },
+    //   this.__stopLoading();
+    // },
     save() {
       if (this.edit) {
         this.update();
@@ -152,6 +266,10 @@ export default {
 
       this.error = false;
       this.errorMessage = "";
+
+      console.log(this.input);
+
+      return this.__stopLoading();
 
       try {
         let res = await this.$service.category.create(this.input);
