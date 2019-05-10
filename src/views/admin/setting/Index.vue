591ca0c7c95e26b47ec87b46c576dcf00f5e3f10
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-05-10 19:30:05 +0700

    setting kalkulasi dimension

--- src/views/admin/setting/Index.vue
+++ src/views/admin/setting/Index.vue
@@ -14,13 +14,14 @@
             </h3>
           </div>
         </div>
-        <div class="uk-width-auto">
-        </div>
+        <div class="uk-width-auto"></div>
       </div>
     </div>
     <div class="uk-card-body uk-card-small">
       <div v-for="(setting, index) in settings" :key="index">
-        <h5 class="uk-heading-line"><span>{{ setting.name }}</span></h5>
+        <h5 class="uk-heading-line">
+          <span>{{ setting.name }}</span>
+        </h5>
         <div v-if="setting.type === 'array'" class="uk-margin-small">
           <div v-for="(val, valIndex) in setting.value" :key="`val_${valIndex}`">
             <div v-for="(itemKey, itemIndex) in Object.keys(val)" :key="`item_${itemIndex}`">
@@ -29,7 +30,7 @@
                   <el-radio v-model="settings[index].value[valIndex][itemKey]" label="1">Active</el-radio>
                   <el-radio v-model="settings[index].value[valIndex][itemKey]" label="0">Inactive</el-radio>
                 </div>
-                <div  v-else>
+                <div v-else>
                   <el-input v-model="settings[index].value[valIndex][itemKey]" size="small">
                     <div slot="prepend" class="uk-width-medium">{{ itemKey }}</div>
                   </el-input>
@@ -43,17 +44,42 @@
           </div>
         </div>
         <div v-if="setting.type === 'object'" class="uk-margin-small">
-          <div v-for="(itemKey, itemIndex) in Object.keys(setting.value)" :key="`item_${itemIndex}`">
+          <div
+            v-for="(itemKey, itemIndex) in Object.keys(setting.value)"
+            :key="`item_${itemIndex}`"
+          >
             <div class="uk-margin-small">
-              <el-input v-model="settings[index].value[itemKey]" size="small">
+              <div v-if="itemKey == 'active'">
+                <el-radio
+                  v-model="settings[index].value[itemKey]"
+                  :label="actv_true"
+                  @change="onActiveChanged(true)"
+                >Active</el-radio>
+                <el-radio
+                  v-model="settings[index].value[itemKey]"
+                  :label="actv_false"
+                  @change="onActiveChanged(false)"
+                >Inactive</el-radio>
+              </div>
+              <div v-else>
+                <el-input
+                  :disabled="(dimension.active == 'false') && ((itemKey == 'regular') || (itemKey == 'premium') || (itemKey == 'default'))"
+                  v-model="settings[index].value[itemKey]"
+                  size="small"
+                >
+                  <div slot="prepend" class="uk-width-medium">{{ itemKey }}</div>
+                </el-input>
+              </div>
+
+              <!-- <el-input v-model="settings[index].value[itemKey]" size="small">
                 <div slot="prepend" class="uk-width-medium">{{ itemKey }}</div>
-              </el-input>
+              </el-input>-->
             </div>
           </div>
         </div>
-          <div class="uk-text-right">
-            <el-button type="primary" @click="addRow(index)">Add Row</el-button>
-          </div>
+        <div class="uk-text-right">
+          <el-button type="primary" @click="addRow(index)">Add Row</el-button>
+        </div>
       </div>
     </div>
     <div class="uk-card-footer">
@@ -66,66 +92,90 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      settings: []
-    }
+      settings: [],
+      dimension: {
+        active: ""
+      },
+      actv_true: true,
+      actv_false: false
+    };
   },
 
-  async created () {
-    this.__startLoading()
+  async created() {
+    this.__startLoading();
 
-    await this.fetchConfigs()
+    await this.fetchConfigs();
 
-    this.__stopLoading()
+    this.__stopLoading();
   },
 
   methods: {
-    async fetchConfigs () {
+    async fetchConfigs() {
       try {
-        let res = await this.$service.config.all()
+        let res = await this.$service.config.all();
 
-        this.settings = res.data
-        console.log(this.settings)
+        this.settings = res.data;
+        console.log(this.settings);
       } catch (err) {
-        this.__handleError(this, err, true)
+        this.__handleError(this, err, true);
       }
     },
-    async update () {
-      this.__startLoading()
+    onActiveChanged(active) {
+      if (!active) {
+        this.dimension.active = "false";
+      } else {
+        this.dimension.active = "true";
+      }
+      console.log(active);
+
+      // return active;
+    },
+    async update() {
+      this.__startLoading();
 
       try {
-        let res = await this.$service.config.update(this.settings)
+        let res = await this.$service.config.update(this.settings);
 
         this.$notify({
-          title: 'SUCCESS',
+          title: "SUCCESS",
           message: res.data.message,
-          type: 'success'
-        })
+          type: "success"
+        });
       } catch (err) {
-        this.__handleError(this, err, true)
+        this.__handleError(this, err, true);
       }
 
-      this.__stopLoading()
+      this.__stopLoading();
     },
-    addRow (test) {
+    addRow(test) {
       switch (this.settings[test].name) {
-        case 'bank_accounts':
-          this.settings[test].value.push({"bank":"","account_number":"","name":"","active_status":"0"})
+        case "bank_accounts":
+          this.settings[test].value.push({
+            bank: "",
+            account_number: "",
+            name: "",
+            active_status: "0"
+          });
           break;
-        case 'transfer_bank':
-          this.settings[test].value.push({"bank":"","account_number":"","name":"","active_status":"0"})
+        case "transfer_bank":
+          this.settings[test].value.push({
+            bank: "",
+            account_number: "",
+            name: "",
+            active_status: "0"
+          });
           break;
-        case 'paypal':
-
+        case "paypal":
           break;
-        case 'credit_card':
+        case "credit_card":
           break;
       }
     },
-    delRow (x,y) {
-      this.settings[x].value.splice(y,1)
+    delRow(x, y) {
+      this.settings[x].value.splice(y, 1);
     }
   }
-}
+};
 </script>
