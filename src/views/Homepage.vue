a9ce692c7f1e44a9dda3bfe42613019cea0139dc
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-10-03 10:55:27 +0700

    first commit

---
+++ src/views/Homepage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h1>Homepage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+    methods: {
+        fetchUsers() {
+            let endpoint = '/get/users';
+            this.axios.get(endpoint).then(res => {
+                console.log('masuk pak eko', res);
+            }).catch(err => {
+                console.log('gamasuk pak eko', err);
+            })
+        }
+    },
+    created() {
+        // this.fetchUsers();
+    }
+
+}
+</script>
+
+<style>
+
+</style>
