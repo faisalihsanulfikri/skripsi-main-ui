8217b14994f17322a2e3bff2b43e4fd499d69717
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-10-12 12:18:18 +0700

    fix

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>
-      <h1>Homepage</h1>
-  </div>
+    <div>
+        <h1>Homepage</h1>
+    </div>
 </template>
 
 <script>
 export default {
 
     methods: {
-        fetchUsers() {
+        getUsers() {
             let endpoint = '/get/users';
             this.axios.get(endpoint).then(res => {
                 console.log('masuk pak eko', res);
             }).catch(err => {
                 console.log('gamasuk pak eko', err);
             })
+
+            
+        },
+        getWebsite(id) {
+            let endpoint = `/get/website/${id}`;
+            this.axios.get(endpoint).then(res => {
+                console.log('masuk website', res);
+            }).catch(err => {
+                console.log('gamasuk website', err);
+            })
         }
     },
     created() {
-        // this.fetchUsers();
+        this.getUsers();
+        this.getWebsite(2);
     }
 
 }
