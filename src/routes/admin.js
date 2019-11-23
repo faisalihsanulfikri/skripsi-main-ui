996d6e8805a69b33aa2ceacc7cbc904262a949ac
faisal.ihsanulfikri <faisalihsanulfikri@gmail.com>
2019-11-23 19:45:46 +0700

    crud website

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,39 +1,7 @@
 import Main from "../views/admin/Main";
 
-// import AwbShow from '../views/agent/awb/Show'
 import Dashboard from "../views/admin/Dashboard";
-// import Invoice from '../views/admin/invoice/Index'
-// import mInvoice from '../views/admin/invoice/Membership'
-// import Inbound from '../views/admin/order/Inbound'
-// import Category from '../views/admin/category/Index'
-// import CategoryCreate from '../views/admin/category/Create'
-// import Warehouse from '../views/admin/warehouse/Index'
-// import WarehouseCreate from '../views/admin/warehouse/Create'
-// import Exchange from '../views/admin/exchange-rate/Index'
-// import ExchangeCreate from '../views/admin/exchange-rate/Create'
-// import Area from '../views/admin/area/Index'
-// import AreaCity from '../views/admin/area/City'
-// import AreaDistrict from '../views/admin/area/District'
-// import AreaSubDistrict from '../views/admin/area/subdistrict/Index'
-// import AreaSubDistrictEdit from '../views/admin/area/subdistrict/Create'
-// import AreaIndexCity from '../views/admin/area/city/Index'
-// import AreaCode from '../views/admin/area/location/Index'
-// import AreaCodeCreate from '../views/admin/area/location/Create'
-// import User from '../views/admin/user/Index'
-// import UserCreate from '../views/admin/user/Create'
-// import UserEdit from '../views/admin/user/Edit'
-// import UserDetail from '../views/admin/user/Detail'
-// import Sales from '../views/admin/report/Sales'
-// import Setting from '../views/admin/setting/Index'
-// import PromoCode from '../views/admin/master/promo-code/Index'
-// import PromoCodeCreate from '../views/admin/master/promo-code/Create'
-// import Membership from '../views/admin/Membership/Index'
-// import MembershipCreate from '../views/admin/Membership/Create'
-// import MemberShipedit from '../views/admin/Membership/Edit'
-// import Inboundnew from '../views/admin/inbound/new'
-// import Inboundinprogres from '../views/admin/inbound/inprogres'
-// import Inboundconsolidate from '../views/admin/inbound/consolidate'
-// import Inboundnoteissues from '../views/admin/inbound/noteissues'
+import WebsiteIndex from "../views/admin/website/Index";
 
 import { ADMIN, SUPER_ADMIN } from "../config/level";
 
@@ -41,6 +9,24 @@ export default {
   path: "/admin",
   component: Main,
   children: [
+    {
+      path: "/",
+      name: "admin-main",
+      component: Dashboard,
+      meta: {
+        auth: true,
+        level: [ADMIN, SUPER_ADMIN]
+      }
+    },
+    {
+      path: "/websites",
+      name: "admin-main",
+      component: Dashboard,
+      meta: {
+        auth: true,
+        level: [ADMIN, SUPER_ADMIN]
+      }
+    },
     {
       path: "/",
       name: "admin-main",
--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,82 +1,17 @@
 <template>
-  <div class="uk-card uk-card-default">
-    <!-- Header -->
-    <PageTitle title="Dashboard" />
-
-    <!-- Content -->
-    <el-card>
-      <el-row :gutter="20" type="flex" justify="left" class="base-chart-row">
-        <el-col :xs="24" :sm="24" :md="10">
-          <el-card>
-            <ChartBar
-              :styles="chartBarStyle"
-              label="ORDERS"
-              :labels="dates"
-              :data="total"
-            />
-          </el-card>
-        </el-col>
-        <el-col :xs="24" :sm="24" :md="10">
-          <el-card>
-            <ChartBar
-              :styles="chartBarStyle"
-              label="PAYMENT"
-              :labels="dates"
-              :data="amount"
-            />
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-card>
+  <div>
+    <h1>Dashboard</h1>
   </div>
 </template>
 
 <script>
-import PageTitle from "@/components/PageTitle";
-import ChartBar from "@/components/charts/ChartBar";
-import ChartDoughnut from "@/components/charts/ChartDoughnut";
-import moment from "moment";
-
 export default {
-  components: {
-    PageTitle,
-    ChartBar,
-    ChartDoughnut
-  },
+  components: {},
   data() {
-    return {
-      amount: [],
-      dates: [],
-      total: []
-    };
+    return {};
   },
-  computed: {
-    chartBarStyle() {
-      return {
-        height: "400px",
-        width: "400px",
-        position: "relative"
-      };
-    }
-  },
-  methods: {
-    async chart() {
-      this.__startLoading();
-
-      try {
-        const res = await this.$service.chart.get();
-        const resData = res.data.data;
-
-        this.dates = resData.map(el => moment(el.date).format("DD-MMM"));
-        this.amount = resData.map(el => el.amount);
-        this.total = resData.map(el => el.total);
-      } catch (e) {
-        this.__handleError(this, err, true);
-      }
-
-      this.__stopLoading();
-    }
-  },
+  computed: {},
+  methods: {},
   async created() {
     // let chart = await this.chart();
   }
